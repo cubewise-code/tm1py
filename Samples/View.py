acef9c328cd6990b785f7d2b9ba70b5712841976
MariusWirtz <MariusWirtz2@gmail.com>
2015-11-12 10:55:06 +0100

    Update View.py

--- Samples/View.py
+++ Samples/View.py
@@ -6,21 +6,22 @@ q = TM1Queries(ip='', port=8008, user='admin', password='apple', ssl=True)
 
 # create native view
 random_string = str(uuid.uuid4())
-view = NativeView('plan_Budgetplan', random_string)
-view.add_row('plan_version', 'FY 2004 Budget')
-view.add_row('plan_business_unit', 'n level business unit')
-view.add_row('plan_department', 'n level departments')
-view.add_row('plan_chart_of_accounts', 'Consolidations')
-view.add_title('plan_exchange_rates', 'actual', 'actual')
-view.add_row('plan_source', 'budget')
-view.add_column('plan_time', '2003 Total Year')
+view = NativeView(name_cube='plan_Budgetplan', name_view=random_string)
+
+# assign subsets to rows, columns, titles
+view.add_row(dimension_name='plan_version', subset_name='FY 2004 Budget')
+view.add_row(dimension_name='plan_business_unit', subset_name='n level business unit')
+view.add_row(dimension_name='plan_department', subset_name='n level departments')
+view.add_row(dimension_name='plan_chart_of_accounts', subset_name='Consolidations')
+view.add_row(dimension_name='plan_source', subset_name='budget')
+view.add_title(dimension_name='plan_exchange_rates', subset_name='actual', selection='actual')
+view.add_column(dimension_name='plan_time', subset_name='2003 Total Year')
+
 # post native view against TM1 Server
 q.create_view(view)
 
-
 # delete view from server
 q.delete_view(view.get_cube(), view.get_name())
-
 
 # create mdx view
 random_string = str(uuid.uuid4())
@@ -28,13 +29,12 @@ mdx = "SELECT {([plan_version].[FY 2003 Budget], [plan_department].[105], [plan_
       "[plan_exchange_rates].[local], [plan_source].[goal] , [plan_time].[Jan-2004]) } on COLUMNS," \
       "{[plan_business_unit].[10110]} on ROWS FROM [plan_BudgetPlan]"
 mdx_view = MDXView('plan_Budgetplan', random_string, mdx)
-# post mdx view against TM1 Server
-q.create_view(mdx_view)
 
+# create view on TM1 Server
+q.create_view(mdx_view)
 
 # delete view from server
 q.delete_view(mdx_view.get_cube(), mdx_view.get_name())
-
 
 # logout
 q.logout()
