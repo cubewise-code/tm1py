e1e9701dbaae4e6c7d6dfc96adbe6061767e2112
MariusWirtz <MariusWirtz2@gmail.com>
2016-02-18 23:53:20 +0100

    Update Subset.py

--- Samples/Subset.py
+++ Samples/Subset.py
@@ -1,24 +1,24 @@
 import uuid
-from TM1py import Subset, TM1Queries
+from TM1py import Subset, TM1Queries as TM1
 
 random_string1 = str(uuid.uuid4())
 random_string2 = str(uuid.uuid4())
 
-q = TM1Queries(ip='', port=8008, user='admin', password='apple', ssl=True)
+tm1 = TM1(ip='localhost', port=8001, user='admin', password='apple', ssl=False)
 
 # create dynamic subset
-s = Subset(dimension_name='plan_business_unit', subset_name=random_string1,
+s = Subset(dimension_name='plan_business_unit', subset_name='TM1py_' + random_string1,
            expression='{ HIERARCHIZE( {TM1SUBSETALL( [plan_business_unit] )} ) }')
-response = q.create_subset(s)
+tm1.create_subset(s)
 
 # create static subset
-s = Subset(dimension_name='plan_business_unit', subset_name=random_string2,
+s = Subset(dimension_name='plan_business_unit', subset_name='TM1py_' + random_string2,
            elements=['10000', '10000', '10000', '10000', '10000', '10000', '10000', '10000', '10000'])
-response = q.create_subset(s)
+tm1.create_subset(s)
 
 # delete subset
-q.delete_subset(name_dimension='plan_business_unit', name_subset=random_string1)
-q.delete_subset(name_dimension='plan_business_unit', name_subset=random_string2)
+tm1.delete_subset(dimension_name='plan_business_unit', subset_name='TM1py_' + random_string1)
+tm1.delete_subset(dimension_name='plan_business_unit', subset_name='TM1py_' + random_string2)
 
 # logout
-q.logout()
+tm1.logout()
