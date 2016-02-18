f9a4ef16591f6193923472952e4d769f664547a5
MariusWirtz <MariusWirtz2@gmail.com>
2016-02-18 23:59:28 +0100

    Update Subset.py

--- Tests/Subset.py
+++ Tests/Subset.py
@@ -1,58 +1,58 @@
-__author__ = 'Marius Wirtz'
-
-from TM1py import TM1Queries, Subset
+from TM1py import TM1Queries as TM1, Subset
 import uuid
-import json
 import unittest
 
 
 class TestAnnotationMethods(unittest.TestCase):
-    q = TM1Queries(ip='', port=8008, user='admin', password='apple', ssl=True)
-    random_string1 = str(uuid.uuid4()).replace('-', '_')
-    random_string2 = str(uuid.uuid4()).replace('-', '_')
+    tm1 = TM1(ip='localhost', port=8001, user='admin', password='apple', ssl=False)
+
+    random_string = str(uuid.uuid4())
+    subset_name_static = 'TM1py_unittest_static_subset_' + random_string
+    subset_name_dynamic = 'TM1py_unittest_dynamic_subset_' + random_string
 
     # 1. create subset
-    def test_create_subset(self):
-        s = Subset(dimension_name='plan_business_unit', subset_name=self.random_string1,
+    def test_1create_subset(self):
+        s = Subset(dimension_name='plan_business_unit',
+                   subset_name=self.subset_name_static,
                    elements=['10110', '10300', '10210', '10000'])
-        response = self.q.create_subset(s)
-        print(response)
-        response_as_dict = json.loads(response)
-        name_in_response = response_as_dict['Name']
-        self.assertEqual(self.random_string1, name_in_response)
+        self.tm1.create_subset(s)
 
-        s = Subset(dimension_name='plan_business_unit', subset_name=self.random_string2,
+        s = Subset(dimension_name='plan_business_unit',
+                   subset_name=self.subset_name_dynamic,
                    expression='{ HIERARCHIZE( {TM1SUBSETALL( [plan_business_unit] )} ) }')
-        response = self.q.create_subset(s)
-        response_as_dict = json.loads(response)
-        name_in_response = response_as_dict['Name']
-        self.assertEqual(self.random_string2, name_in_response)
+        self.tm1.create_subset(s)
 
     # 2. get subset
-    def test_get_subset(self):
-        s = self.q.get_subset(dimension_name='plan_business_unit', subset_name='static_subset_for_unit_test')
+    def test_2get_subset(self):
+        s = self.tm1.get_subset(dimension_name='plan_business_unit',
+                              subset_name=self.subset_name_static)
         self.assertIsInstance(s, Subset)
 
-        s = self.q.get_subset(dimension_name='plan_business_unit', subset_name='dynamic_subset_for_unit_test')
+        s = self.tm1.get_subset(dimension_name='plan_business_unit',
+                              subset_name=self.subset_name_dynamic)
         self.assertIsInstance(s, Subset)
 
     # 3. update subset
-    def test_update_subset(self):
-        s = self.q.get_subset(dimension_name='plan_business_unit', subset_name='static_subset_for_unit_test')
+    def test_3update_subset(self):
+        s = self.q.get_subset(dimension_name='plan_business_unit',
+                              subset_name=self.subset_name_static)
         s.add_elements(['10110'])
-        self.q.update_subset(s)
+        self.tm1.update_subset(s)
 
-        s = self.q.get_subset(dimension_name='plan_business_unit', subset_name='dynamic_subset_for_unit_test')
+        s = self.tm1.get_subset(dimension_name='plan_business_unit',
+                              subset_name=self.subset_name_dynamic)
         s.set_expression('{ HIERARCHIZE( {TM1SUBSETALL( [plan_business_unit] )} ) }')
-        self.q.update_subset(s)
+        self.tm1.update_subset(s)
 
     # 4. delete subset
-    def test_delete_subset(self):
-        response = self.q.delete_subset('plan_business_unit', self.random_string1)
-        self.assertEqual(response, '')
-        response = self.q.delete_subset('plan_business_unit', self.random_string2)
-        self.assertEqual(response, '')
+    def test_4delete_subset(self):
+        self.tm1.delete_subset(dimension_name='plan_business_unit',
+                             subset_name=self.subset_name_static)
+        self.tm1.delete_subset(dimension_name='plan_business_unit',
+                             subset_name=self.subset_name_dynamic)
 
+    def test_5logout(self):
+        self.tm1.logout()
 
 if __name__ == '__main__':
     unittest.main()
