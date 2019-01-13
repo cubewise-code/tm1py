222b19e23de5642860cecc44b6bcc64b6ce40afc
Marius Wirtz <MariusWirtz2@gmail.com>
2019-01-13 19:33:24 +0100

    new remove_element method method removes all related edges as well

--- Tests/Hierarchy.py
+++ Tests/Hierarchy.py
@@ -56,17 +56,7 @@ class TestHierarchyMethods(unittest.TestCase):
                    expression="{{[{}].Members}}".format(DIMENSION_NAME))
         cls.tm1.dimensions.subsets.create(s, False)
 
-    def test_get_hierarchy(self):
-        h = self.tm1.dimensions.hierarchies.get(DIMENSION_NAME, DIMENSION_NAME)
-        self.assertIn('Total Years', h.elements.keys())
-        self.assertIn('No Year', h.elements.keys())
-        self.assertIn('1989', h.elements.keys())
-        self.assertIn('Next Year', [ea.name for ea in h.element_attributes])
-        self.assertIn('Previous Year', [ea.name for ea in h.element_attributes])
-        self.assertIn(SUBSET_NAME, h.subsets)
-
-    def test_update_hierarchy(self):
-        # Get dimension and hierarchy
+    def update_hierarchy(self):
         d = self.tm1.dimensions.get(dimension_name=DIMENSION_NAME)
         h = d.default_hierarchy
         # Edit Elements and Edges
@@ -92,6 +82,18 @@ class TestHierarchyMethods(unittest.TestCase):
         h.update_element('No Year', 'String')
         self.tm1.dimensions.update(d)
 
+    def test_get_hierarchy(self):
+        h = self.tm1.dimensions.hierarchies.get(DIMENSION_NAME, DIMENSION_NAME)
+        self.assertIn('Total Years', h.elements.keys())
+        self.assertIn('No Year', h.elements.keys())
+        self.assertIn('1989', h.elements.keys())
+        self.assertIn('Next Year', [ea.name for ea in h.element_attributes])
+        self.assertIn('Previous Year', [ea.name for ea in h.element_attributes])
+        self.assertIn(SUBSET_NAME, h.subsets)
+
+    def test_update_hierarchy(self):
+        self.update_hierarchy()
+
         # Check if update works
         d = self.tm1.dimensions.get(DIMENSION_NAME)
         h = d.default_hierarchy
@@ -112,6 +114,99 @@ class TestHierarchyMethods(unittest.TestCase):
         self.assertEqual(summary["Members"], 147)
         self.assertEqual(summary["ElementAttributes"], 4)
         self.assertEqual(summary["Levels"], 3)
+
+    def test_update_hierarchy_remove_c_element(self):
+        self.update_hierarchy()
+
+        d = self.tm1.dimensions.get(DIMENSION_NAME)
+        h = d.default_hierarchy
+        self.assertIn('2011', h.elements)
+        self.assertIn(('2011', '2011-Jan'), h.edges)
+
+        h.remove_element('2011')
+        self.tm1.dimensions.hierarchies.update(h)
+
+        d = self.tm1.dimensions.get(DIMENSION_NAME)
+        h = d.default_hierarchy
+        self.assertNotIn('2011', h.elements)
+        self.assertNotIn(('2011', '2011-Jan'), h.edges)
+
+    def test_update_hierarchy_remove_n_element(self):
+        self.update_hierarchy()
+
+        d = self.tm1.dimensions.get(DIMENSION_NAME)
+        h = d.default_hierarchy
+        self.assertIn('2011-Jan', h.elements)
+        self.assertIn(('2011', '2011-Jan'), h.edges)
+
+        h.remove_element('2011-Jan')
+        self.tm1.dimensions.hierarchies.update(h)
+
+        d = self.tm1.dimensions.get(DIMENSION_NAME)
+        h = d.default_hierarchy
+        self.assertNotIn('2011-Jan', h.elements)
+        self.assertNotIn(('2011', '2011-Jan'), h.edges)
+
+    def test_update_hierarchy_remove_s_element(self):
+        self.update_hierarchy()
+
+        d = self.tm1.dimensions.get(DIMENSION_NAME)
+        h = d.default_hierarchy
+        self.assertIn('No Year', h.elements)
+
+        h.remove_element('No Year')
+        self.tm1.dimensions.hierarchies.update(h)
+
+        d = self.tm1.dimensions.get(DIMENSION_NAME)
+        h = d.default_hierarchy
+        self.assertNotIn('No Year', h.elements)
+
+    def test_update_hierarchy_remove_edges_related_to_element(self):
+        self.update_hierarchy()
+
+        d = self.tm1.dimensions.get(DIMENSION_NAME)
+        h = d.default_hierarchy
+        self.assertIn('2012', h.elements)
+
+        h.remove_edges_related_to_element(element_name='2012 ')
+        self.tm1.dimensions.hierarchies.update(h)
+
+        d = self.tm1.dimensions.get(DIMENSION_NAME)
+        h = d.default_hierarchy
+        self.assertIn('2012', h.elements)
+        self.assertNotIn(('2012', '2012- Jan'), h.edges)
+        self.assertNotIn(('2012', '2012-DEC'), h.edges)
+        self.assertNotIn(('TotalYears', '2012'), h.edges)
+        self.assertIn(('Total YEARS', '2011'), h.edges)
+        self.assertIn(('Total Years', '2013'), h.edges)
+
+    def test_update_hierarchy_remove_edges(self):
+        self.update_hierarchy()
+
+        d = self.tm1.dimensions.get(DIMENSION_NAME)
+        h = d.default_hierarchy
+        self.assertIn('2012', h.elements)
+        self.assertIn(('2012', '2012-Jan'), h.edges)
+        self.assertIn(('2012', '2012-Feb'), h.edges)
+        self.assertIn(('2012', '2012-Mar'), h.edges)
+        self.assertIn(('2012', '2012-Apr'), h.edges)
+
+        edges = [('2012', '2012- Jan'), ('2012', '2012-Feb'), ('2012', '2012-MAR'), ('2012', '2012-Apr')]
+        h.remove_edges(edges=edges)
+        self.tm1.dimensions.hierarchies.update(h)
+
+        d = self.tm1.dimensions.get(DIMENSION_NAME)
+        h = d.default_hierarchy
+
+        self.assertNotIn(('2012', '2012-Jan'), h.edges)
+        self.assertNotIn(('2012', '2012-Feb'), h.edges)
+        self.assertNotIn(('2012', '2012-Mar'), h.edges)
+        self.assertNotIn(('2012', '2012-Apr'), h.edges)
+        self.assertNotIn(('2012', '2012 - JAN'), h.edges)
+        self.assertIn(('2012', '2012-May'), h.edges)
+
+        self.assertIn('2012', h.elements)
+        self.assertIn('2012-Feb', h.elements)
 
     def test_hierarchy_summary(self):
         summary = self.tm1.dimensions.hierarchies.get_hierarchy_summary(DIMENSION_NAME, DIMENSION_NAME)
