354d8f0c1c03d0eb44889b2512eba3bccf29cd6d
Marius Wirtz <mwirtz@cubewise.com>
2019-05-19 22:11:04 +0200

    Release 1.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import codecs
 
 from setuptools import setup
 
-SCHEDULE_VERSION = '1.2.2'
+SCHEDULE_VERSION = '1.3.0'
 SCHEDULE_DOWNLOAD_URL = (
         'https://github.com/Cubewise-code/TM1py/tarball/' + SCHEDULE_VERSION
 )
