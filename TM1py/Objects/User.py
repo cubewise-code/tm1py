72cc178221560257689b92f090eff1a3dd2a7f0a
Ryan Clapp <{ID}+{username}@users.noreply.github.com>
2020-04-23 22:41:09 -0700

    Added Usertype enum to match the TM1 implementation

--- TM1py/Objects/User.py
+++ TM1py/Objects/User.py
@@ -3,10 +3,17 @@
 import collections
 import json
 from typing import Iterable, Optional, List, Dict
+from enum import Enum
 
 from TM1py.Objects.TM1Object import TM1Object
 from TM1py.Utils.Utils import CaseAndSpaceInsensitiveSet, format_url
 
+class UserType(Enum):
+    User = 0
+    SecurityAdmin = 1
+    DataAdmin = 2
+    Admin = 3
+    OperationsAdmin = 4
 
 class User(TM1Object):
     """ Abstraction of a TM1 User
@@ -14,16 +21,36 @@ class User(TM1Object):
     """
 
     def __init__(self, name: str, groups: Iterable[str], friendly_name: Optional[str] = None,
-                 password: Optional[str] = None, enabled: Optional[bool] = True):
+                 password: Optional[str] = None, enabled: Optional[bool] = True, user_type: Optional[str] = None):
         self._name = name
         self._groups = CaseAndSpaceInsensitiveSet(*groups)
         self._friendly_name = friendly_name
         self._password = password
         self._enabled = enabled
 
+        if user_type is None:
+            if 'Admin' in self._groups:
+                self._user_type = UserType.admin
+            elif 'SecurityAdmin' in self._groups:
+                self._user_type = UserType.securityAdmin
+            elif 'DataAdmin' in self._groups:
+                self._user_type = UserType.dataAdmin
+            elif 'OperationsAdmin' in self._groups:
+                self._user_type = UserType.OperationsAdmin
+            else:
+                self._user_type = UserType.User
+        else:
+            self._user_type = UserType[user_type]
+
+
+
     @property
     def name(self) -> str:
         return self._name
+
+    @property
+    def user_type(self) -> UserType:
+        return self._user_type
 
     @property
     def friendly_name(self) -> str:
@@ -60,8 +87,12 @@ class User(TM1Object):
         self._password = value
 
     @enabled.setter
-    def password(self, value: bool):
+    def enabled(self, value: bool):
         self._enabled = value
+
+    @user_type.setter
+    def user_type(self, value:str):
+        self._user_type = UserType[value]
 
     def add_group(self, group_name: str):
         self._groups.add(group_name)
@@ -89,7 +120,8 @@ class User(TM1Object):
         return cls(name=user_as_dict['Name'],
                    friendly_name=user_as_dict['FriendlyName'],
                    groups=[group["Name"] for group in user_as_dict['Groups']],
-                   enabled= user_as_dict['Enabled'])
+                   enabled= user_as_dict['Enabled'],
+                   user_type=user_as_dict['Type'])
 
     @property
     def body(self) -> str:
@@ -109,4 +141,5 @@ class User(TM1Object):
                                              for group
                                              in self.groups]
         body_as_dict['Enabled'] = self._enabled
+        body_as_dict['Type'] = self._user_type
         return json.dumps(body_as_dict, ensure_ascii=False)
