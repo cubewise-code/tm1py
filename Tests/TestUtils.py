e3e0c8f05e8bf245ffdc99497e698696b51ae46b
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-08-08 20:31:50 +0200

    re-formatted

--- Tests/TestUtils.py
+++ Tests/TestUtils.py
@@ -1,14 +1,18 @@
 import functools
+
 from TM1py.Utils.Utils import verify_version
+
 
 def skip_if_no_pandas(func):
     """ 
     Checks whether pandas is installed and skips the test if not
     """
+
     @functools.wraps(func)
     def wrapper(self, *args, **kwargs):
         try:
             import pandas
+
             return func(self, *args, **kwargs)
         except:
             return self.skipTest(f"Test '{func.__name__}' requires pandas")
@@ -25,7 +29,9 @@ def skip_if_insufficient_version(version):
         @functools.wraps(func)
         def wrapper(self, *args, **kwargs):
             if not verify_version(required_version=version, version=self.tm1.version):
-                return self.skipTest(f"Function '{ func.__name__, }' requires TM1 server version >= '{ version }'")
+                return self.skipTest(
+                    f"Function '{ func.__name__, }' requires TM1 server version >= '{ version }'"
+                )
             else:
                 return func(self, *args, **kwargs)
 
