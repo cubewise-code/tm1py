e5d287652954f70cc3c7409f53dcd32b26de3f86
vinoth kumar <rkvinoth@amazon.com>
2020-08-10 19:37:19 +0530

    remove_edges_under_consolidation

--- TM1py/Services/HierarchyService.py
+++ TM1py/Services/HierarchyService.py
@@ -211,6 +211,30 @@ class HierarchyService(ObjectService):
         }
         return self._rest.PATCH(url=url, data=json.dumps(body), **kwargs)
 
+    def remove_edges_under_consolidation(self, dimension_name: str,  hierarchy_name: str,
+                                         consolidation_element: str, **kwargs) -> Response:
+        """
+        :param dimension_name: Name of the dimension
+        :param hierarchy_name: Nmae of the hierarchy
+        :param consolidation_element: Name of the Consolidated element
+        :return: response
+        """
+        hierarchy = self.get(dimension_name, hierarchy_name)
+        from TM1py.Services import ElementService
+        element_service = ElementService(self._rest)
+        elements_under_consolidations = element_service.get_members_under_consolidation(dimension_name, hierarchy_name,
+                                                                                        consolidation_element)
+        elements_under_consolidations.append(consolidation_element)
+        remove_edges = []
+        for element, _ in hierarchy.edges.items():
+            if element[0] in elements_under_consolidations and element[1] in elements_under_consolidations:
+                remove_edges.append(element)
+        hierarchy.remove_edges(remove_edges)
+
+        url = format_url("/api/v1/Dimensions('{}')/Hierarchies('{}')", dimension_name, hierarchy_name)
+        response = self._rest.PATCH(url=url, data=hierarchy.body, **kwargs)
+        return response
+
     def add_edges(self, dimension_name: str, hierarchy_name: str = None, edges: Dict[Tuple[str, str], int] = None,
                   **kwargs) -> Response:
         """ Add Edges to hierarchy. Fails if any edge already exists.
