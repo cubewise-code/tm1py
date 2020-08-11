602cd344d48f341dd3f909d8f969221e5ebc0cac
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-08-11 04:22:34 +0200

    clear imports from Tests/__init__.py

--- Tests/__init__.py
+++ Tests/__init__.py
@@ -1,23 +1,5 @@
-from Tests.Annotation import TestAnnotationMethods
-from Tests.Cell import TestDataMethods
-from Tests.Chore import TestChoreMethods
-from Tests.Cube import TestCubeMethods
-from Tests.Dimension import TestDimensionMethods
-from Tests.Element import TestElementMethods
-from Tests.Hierarchy import TestHierarchyMethods
-from Tests.MDXUtils import TestMDXUtils
-from Tests.PowerBiService import TestPowerBiService
-from Tests.Process import TestProcessMethods
-from Tests.Security import TestSecurityMethods
-from Tests.Server import TestServerMethods
-from Tests.Subset import TestSubsetMethods
-from Tests.TIObfuscator import TestTIObfuscatorMethods
-from Tests.Utils import TestUtilsMethods
-from Tests.View import TestViewMethods
-
 """ Notes on TM1py Tests
 - specify your instance coordinates in the config.ini file
 - `EnableSandboxDimension` config parameter must be set to `F` for the target TM1 instance
 - use PyCharm to run the pytests in this folder
-
 """
