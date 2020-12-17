09db40b98bf3801d11d478bbb374552ae230fd5b
Marius Wirtz <MariusWirtz2@gmail.com>
2020-12-17 12:24:29 +0100

    Fix is_admin property with CAM SSO access

--- TM1py/Services/RestService.py
+++ TM1py/Services/RestService.py
@@ -144,7 +144,10 @@ class RestService:
         self._timeout = None if kwargs.get('timeout', None) is None else float(kwargs.get('timeout'))
         self._async_requests_mode = self.translate_to_boolean(kwargs.get('async_requests_mode', False))
         # populated on the fly
-        self._is_admin = True if case_and_space_insensitive_equals(kwargs.get("user", None), "ADMIN") else None
+        if "user" in kwargs:
+            self._is_admin = True if case_and_space_insensitive_equals(kwargs.get("user"), "ADMIN") else None
+        else:
+            self._is_admin = None
 
         if 'verify' in kwargs:
             if isinstance(kwargs['verify'], str):
