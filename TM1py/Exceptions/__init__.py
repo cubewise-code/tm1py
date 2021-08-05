c3b8681a02bc2c8aff188f7175cc8231093c7faa
Marius Wirtz <MariusWirtz2@gmail.com>
2021-08-05 13:52:25 +0200

    include all exceptions in exceptions/__init__.py

--- TM1py/Exceptions/__init__.py
+++ TM1py/Exceptions/__init__.py
@@ -1,1 +1,2 @@
-from TM1py.Exceptions.Exceptions import TM1pyRestException, TM1pyException, TM1pyTimeout
+from TM1py.Exceptions.Exceptions import TM1pyRestException, TM1pyException, TM1pyTimeout, TM1pyVersionException, \
+    TM1pyNotAdminException, TM1pyWriteFailureException, TM1pyWritePartialFailureException
