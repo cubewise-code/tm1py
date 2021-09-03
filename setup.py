9ed6dfd7720f5665184495b5073f257cd75f7be2
Marius Wirtz <MariusWirtz2@gmail.com>
2021-09-03 15:06:31 +0200

    Allow python36 in setup.py

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
