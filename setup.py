1f04ef28d6f8198975f0e37c0d5351220c380ab9
MariusWirtz <MariusWirtz2@gmail.com>
2022-01-20 13:20:37 +0100

    Release 1.9

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-SCHEDULE_VERSION = '1.8.0'
+SCHEDULE_VERSION = '1.9.0'
 SCHEDULE_DOWNLOAD_URL = (
         'https://github.com/Cubewise-code/TM1py/tarball/' + SCHEDULE_VERSION
 )
