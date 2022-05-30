eb274a93f6cc86753eeba2d76b2c87043d758de7
MariusWirtz <MariusWirtz2@gmail.com>
2022-05-30 17:30:22 +0200

    Adjust `get_element_principal_name` function
    Fixes #730

--- TM1py/Services/ElementService.py
+++ TM1py/Services/ElementService.py
@@ -635,11 +635,7 @@ class ElementService(ObjectService):
         response = self._rest.GET(url=url, **kwargs)
 
         return [record["Name"] for record in response.json()["value"]]
-    
 
-    def get_element_principal_name(self, dimension_name: str, hierarchy_name: str, element_name: str, **kwargs) -> Element:
-        url = format_url(
-            "/api/v1/Dimensions('{}')/Hierarchies('{}')/Elements('{}')?$expand=*",
-            dimension_name, hierarchy_name, element_name)
-        response = self._rest.GET(url, **kwargs)
-        return Element.from_dict(response.json())
+    def get_element_principal_name(self, dimension_name: str, hierarchy_name: str, element_name: str, **kwargs) -> str:
+        element = self.get(dimension_name, hierarchy_name, element_name, **kwargs)
+        return element.name
