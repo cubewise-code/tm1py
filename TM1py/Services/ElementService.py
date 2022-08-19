022e5acefbed1fa03a4b53d16455a5454c1c2921
AndrewScheevel <31583660+adscheevel@users.noreply.github.com>
2022-08-19 13:48:30 -0500

    get consolidated elements
    2 functions to get consolidated elements and names

--- TM1py/Services/ElementService.py
+++ TM1py/Services/ElementService.py
@@ -97,6 +97,21 @@ class ElementService(ObjectService):
         response = self._rest.GET(url, **kwargs)
         return [e["Name"] for e in response.json()['value']]
 
+    def get_consolidated_elements(self, dimension_name: str, hierarchy_name: str, **kwargs) -> List[Element]:
+        url = format_url(
+            "/api/v1/Dimensions('{}')/Hierarchies('{}')/Elements?$expand=*&$filter=Type eq 3",
+            dimension_name,
+            hierarchy_name)
+        response = self._rest.GET(url, **kwargs)
+        return [Element.from_dict(element) for element in response.json()["value"]] 
+
+    def get_consolidated_element_names(self, dimension_name: str, hierarchy_name: str, **kwargs) -> List[str]:
+        url = format_url("/api/v1/Dimensions('{}')/Hierarchies('{}')/Elements?$select=Name&$filter=Type eq 3",
+                         dimension_name,
+                         hierarchy_name)
+        response = self._rest.GET(url, **kwargs)
+        return [e["Name"] for e in response.json()['value']]
+
     def get_element_names(self, dimension_name: str, hierarchy_name: str, **kwargs) -> List[str]:
         """ Get all element names
 
