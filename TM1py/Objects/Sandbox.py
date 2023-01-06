3e71e7410d2cfbe5b1a6afe1b593119a66bd11e1
AndrewScheevel <31583660+adscheevel@users.noreply.github.com>
2023-01-06 16:51:52 -0600

    added properties
    added loaded, active, & queued properties

--- TM1py/Objects/Sandbox.py
+++ TM1py/Objects/Sandbox.py
@@ -12,14 +12,25 @@ class Sandbox(TM1Object):
         
     """
 
-    def __init__(self, name: str, include_in_sandbox_dimension: bool = True):
+    def __init__(
+        self, 
+        name: str, 
+        include_in_sandbox_dimension: bool = True,
+        loaded: bool = False,
+        active: bool = False,
+        queued: bool = False
+    ):
         """
         
         :param name: name of the Sandbox
-        :param include_in_sandbox_dimension: 
+        :param include_in_sandbox_dimension:
+        :params loaded, active, queued: leave default as false when creating sanbox 
         """
         self.name = name
         self.include_in_sandbox_dimension = include_in_sandbox_dimension
+        self.loaded = loaded
+        self.active = active
+        self.queued = queued
 
     @property
     def name(self) -> str:
@@ -57,6 +68,9 @@ class Sandbox(TM1Object):
         return cls(
             name=sandbox_as_dict["Name"],
             include_in_sandbox_dimension=sandbox_as_dict["IncludeInSandboxDimension"],
+            loaded=sandbox_as_dict["IsLoaded"],
+            active=sandbox_as_dict["IsActive"],
+            queued=sandbox_as_dict["IsQueued"]
         )
 
     @property
