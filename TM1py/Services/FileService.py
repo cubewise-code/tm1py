1e0d54109a6b9a6d25b24de8140205e4a3a4e955
MariusWirtz <MariusWirtz2@gmail.com>
2024-04-04 17:51:11 +0200

    Allow IO of files to subfolders in v12
    Fixes #1078

--- TM1py/Services/FileService.py
+++ TM1py/Services/FileService.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import json
 import warnings
-from typing import List, Iterable
+from pathlib import Path
+from typing import List, Iterable, Union
 
 from TM1py.Services import RestService
 from TM1py.Services.ObjectService import ObjectService
@@ -35,88 +36,158 @@ class FileService(ObjectService):
 
         return self._rest.GET(url, **kwargs).content
 
-    def get_all_names(self, **kwargs) -> List[str]:
+    def get_all_names(self, path: Union[str, Path] = "", **kwargs) -> List[str]:
         """ return list of blob file names
 
+        :param path: path to folder. When empty searches in root
         """
-
-        url = format_url(
-            "/Contents('{version_content_path}')/Contents?$select=Name",
-            version_content_path=self.version_content_path)
+        path = Path(path)
+        url = self._construct_content_url(path, exclude_path_end=False, extension="Contents")
 
         response = self._rest.GET(url, **kwargs).content
-
         return [file['Name'] for file in json.loads(response)['value']]
 
     def get(self, file_name: str, **kwargs) -> bytes:
+        """ Get file
 
-        url = format_url(
-            "/Contents('{version_content_path}')/Contents('{name}')/Content",
-            name=file_name,
-            version_content_path=self.version_content_path)
+        :param file_name: file name in root or path to file
+        """
+        url = self._construct_content_url(
+            path=Path(file_name),
+            exclude_path_end=False,
+            extension="Content")
 
         return self._rest.GET(url, **kwargs).content
 
-    def create(self, file_name: str, file_content: bytes, **kwargs):
-        url = format_url(
-            "/Contents('{version_content_path}')/Contents",
-            version_content_path=self.version_content_path)
+    def _create_folder(self, folder_name: Union[str, Path], **kwargs):
+        """ Create folder
+
+        Can only create 1 folder at a time.
+        To create nested folder structures it must be called in an iterative fashion.
+
+        :param folder_name: path to folder (e.g. folderA, folderA/folderB)
+        """
+        path = Path(folder_name)
+        url = self._construct_content_url(path, exclude_path_end=True, extension="Contents")
+
         body = {
-            "@odata.type": "#ibm.tm1.api.v1.Document",
-            "ID": file_name,
-            "Name": file_name
+            "@odata.type": "#ibm.tm1.api.v1.Folder",
+            "Name": folder_name.name
         }
         self._rest.POST(url, json.dumps(body), **kwargs)
 
+    def _construct_content_url(self, path: Path, exclude_path_end: bool = True, extension: str = 'Contents') -> str:
+        """ Dynamically construct URL to use in FileService functions
+
+        :param path: file name in root or path to file
+        :param exclude_path_end: Some functions require complete URL to file (e.g. exists),
+        while others require URL to parent folder (e.g., create)
+        :param extension: Final piece of the URL e.g. ('Contents', 'Content', or 'Contents?$select=Name')
+
+        """
+        parent_folders = {
+            f"level{str(n).zfill(4)}": parent
+            for n, parent
+            in enumerate(path.parts[:-1] if exclude_path_end else path.parts, 1)
+        }
+
         url = format_url(
-            "/Contents('{version_content_path}')/Contents('{name}')/Content",
-            name=file_name,
-            version_content_path=self.version_content_path)
+            "".join([
+                "/Contents('{version_content_path}')/",
+                "/".join(f"Contents('{{{folder}}}')" for folder in parent_folders) + "/" if parent_folders else "",
+                extension
+            ]),
+            version_content_path=self.version_content_path,
+            **parent_folders)
+
+        return url.rstrip("/")
+
+    def create(self, file_name: Union[str, Path], file_content: bytes, **kwargs):
+        """ Create file
+
+        Folders in file_name (e.g. folderA/folderB/file.csv) will be created implicitly
+
+        :param file_name: file name in root or path to file
+        :param file_content: file_content as bytes or BytesIO
+        """
+        path = Path(file_name)
+
+        # Create folder structure iteratively
+        if path.parents:
+            folder_path = Path()
+            for parent_folder in path.parts[:-1]:
+                folder_path = folder_path.joinpath(parent_folder)
+                self._create_folder(folder_name=folder_path)
+
+        url = self._construct_content_url(path, exclude_path_end=True, extension="Contents")
+        body = {
+            "@odata.type": "#ibm.tm1.api.v1.Document",
+            "ID": path.name,
+            "Name": path.name
+        }
+        self._rest.POST(url, json.dumps(body), **kwargs)
+
+        url = self._construct_content_url(path, exclude_path_end=False, extension="Content")
+        return self._rest.PUT(url, file_content, headers=self.binary_http_header, **kwargs)
+
+    def update(self, file_name: Union[str, Path], file_content: bytes, **kwargs):
+        """ Update existing file
+
+        :param file_name: file name in root or path to file
+        :param file_content: file_content as bytes or BytesIO
+        """
+        url = self._construct_content_url(
+            path=Path(file_name),
+            exclude_path_end=False,
+            extension="Content")
 
         return self._rest.PUT(url, file_content, headers=self.binary_http_header, **kwargs)
 
-    def update(self, file_name: str, file_content: bytes, **kwargs):
-        url = format_url(
-            "/Contents('{version_content_path}')/Contents('{name}')/Content",
-            name=file_name,
-            version_content_path=self.version_content_path)
+    def update_or_create(self, file_name: Union[str, Path], file_content: bytes, **kwargs):
+        """ Create file or update file if it already exists
 
-        return self._rest.PUT(url, file_content, headers=self.binary_http_header, **kwargs)
-
-    def update_or_create(self, file_name: str, file_content: bytes, **kwargs):
+        :param file_name: file name in root or path to file
+        :param file_content: file_content as bytes or BytesIO
+        """
         if self.exists(file_name, **kwargs):
             return self.update(file_name, file_content, **kwargs)
 
         return self.create(file_name, file_content, **kwargs)
 
-    def exists(self, file_name: str, **kwargs):
-        url = format_url(
-            "/Contents('{version_content_path}')/Contents('{name}')",
-            name=file_name,
-            version_content_path=self.version_content_path)
+    def exists(self, file_name: Union[str, Path], **kwargs):
+        """ Check if file exists
+
+        :param file_name: file name in root or path to file
+        """
+        url = self._construct_content_url(
+            path=Path(file_name),
+            exclude_path_end=False,
+            extension="")
 
         return self._exists(url, **kwargs)
 
-    def delete(self, file_name: str, **kwargs):
-        url = format_url(
-            "/Contents('{version_content_path}')/Contents('{name}')",
-            name=file_name,
-            version_content_path=self.version_content_path)
+    def delete(self, file_name: Union[str, Path], **kwargs):
+        """ Delete file
+
+        :param file_name: file name in root or path to file
+        """
+        url = self._construct_content_url(
+            path=Path(file_name),
+            exclude_path_end=False,
+            extension="")
 
         return self._rest.DELETE(url, **kwargs)
 
     def search_string_in_name(self, name_startswith: str = None, name_contains: Iterable = None,
-                              name_contains_operator: str = 'and', **kwargs) -> List[str]:
+                              name_contains_operator: str = 'and', path: Union[Path, str] = "",
+                              **kwargs) -> List[str]:
         """ Return list of blob files that match search critera
 
         :param name_startswith: str, file name begins with (case insensitive)
         :param name_contains: iterable, found anywhere in name (case insensitive)
         :param name_contains_operator: 'and' or 'or'
+        :param path: search in given path or root
         """
-
-        url = format_url(
-            "/Contents('{version_content_path}')/Contents?$select=Name",
-            version_content_path=self.version_content_path)
 
         name_contains_operator = name_contains_operator.strip().lower()
         if name_contains_operator not in ("and", "or"):
@@ -139,8 +210,10 @@ class FileService(ObjectService):
             else:
                 raise ValueError("'name_contains' must be str or iterable")
 
-        url += "&$filter={}".format(f" and ".join(name_filters))
-
+        url = self._construct_content_url(
+            path=Path(path),
+            exclude_path_end=False,
+            extension="Contents?$select=Name&$filter={}".format(f" and ".join(name_filters)))
         response = self._rest.GET(url, **kwargs).content
 
         return list(file['Name'] for file in json.loads(response)['value'])
--- Tests/FileService_test.py
+++ Tests/FileService_test.py
@@ -12,6 +12,9 @@ class TestFileService(unittest.TestCase):
     FILE_NAME1 = "TM1py_unittest_file1"
     FILE_NAME2 = "TM1py_unittest_file2"
 
+    FILE_NAME1_IN_FOLDER = Path("TM1py") / "Tests" / FILE_NAME1
+    FILE_NAME2_IN_FOLDER = Path("TM1py") / "Tests" / FILE_NAME2
+
     def setUp(self) -> None:
         self.config = configparser.ConfigParser()
         self.config.read(Path(__file__).parent.joinpath('config.ini'))
@@ -19,9 +22,12 @@ class TestFileService(unittest.TestCase):
 
         with open(Path(__file__).parent.joinpath('resources', 'file.csv'), "rb") as file:
             self.tm1.files.update_or_create(self.FILE_NAME1, file.read())
+            self.tm1.files.update_or_create(self.FILE_NAME1_IN_FOLDER, file.read())
 
         if self.tm1.files.exists(self.FILE_NAME2):
             self.tm1.files.delete(self.FILE_NAME2)
+        if self.tm1.files.exists(self.FILE_NAME2_IN_FOLDER):
+            self.tm1.files.delete(self.FILE_NAME2_IN_FOLDER)
 
     @skip_if_insufficient_version(version="11.4")
     def test_create_get(self):
@@ -29,6 +35,16 @@ class TestFileService(unittest.TestCase):
             self.tm1.files.update_or_create(self.FILE_NAME1, original_file.read())
 
             created_file = self.tm1.files.get(self.FILE_NAME1)
+
+        with open(Path(__file__).parent.joinpath('resources', 'file.csv'), "rb") as original_file:
+            self.assertEqual(original_file.read(), created_file)
+
+    @skip_if_insufficient_version(version="12")
+    def test_create_get_in_folder(self):
+        with open(Path(__file__).parent.joinpath('resources', 'file.csv'), "rb") as original_file:
+            self.tm1.files.update_or_create(self.FILE_NAME1_IN_FOLDER, original_file.read())
+
+            created_file = self.tm1.files.get(self.FILE_NAME1_IN_FOLDER)
 
         with open(Path(__file__).parent.joinpath('resources', 'file.csv'), "rb") as original_file:
             self.assertEqual(original_file.read(), created_file)
@@ -43,14 +59,36 @@ class TestFileService(unittest.TestCase):
         with open(Path(__file__).parent.joinpath('resources', 'file.csv'), "rb") as original_file:
             self.assertEqual(original_file.read(), created_file)
 
+    @skip_if_insufficient_version(version="12")
+    def test_update_get_in_folder(self):
+        with open(Path(__file__).parent.joinpath('resources', 'file.csv'), "rb") as original_file:
+            self.tm1.files.update(self.FILE_NAME1_IN_FOLDER, original_file.read())
+
+            created_file = self.tm1.files.get(self.FILE_NAME1_IN_FOLDER)
+
+        with open(Path(__file__).parent.joinpath('resources', 'file.csv'), "rb") as original_file:
+            self.assertEqual(original_file.read(), created_file)
+
     @skip_if_insufficient_version(version="11.4")
     def test_get_all_names(self):
         result = self.tm1.files.get_all_names()
         self.assertIn(self.FILE_NAME1, result)
 
+    @skip_if_insufficient_version(version="12")
+    def test_get_all_names_in_folder(self):
+        result = self.tm1.files.get_all_names(path=self.FILE_NAME1_IN_FOLDER.parent)
+        self.assertIn(self.FILE_NAME1, result)
+
     @skip_if_insufficient_version(version="11.4")
     def test_search_string_in_name__name_startswith(self):
         result = self.tm1.files.search_string_in_name(name_startswith=self.FILE_NAME1)
+        self.assertEqual([self.FILE_NAME1], result)
+
+    @skip_if_insufficient_version(version="12")
+    def test_search_string_in_name__name_startswith_in_folder(self):
+        result = self.tm1.files.search_string_in_name(
+            name_startswith=self.FILE_NAME1,
+            path=self.FILE_NAME1_IN_FOLDER.parent)
         self.assertEqual([self.FILE_NAME1], result)
 
     @skip_if_insufficient_version(version="11.4")
@@ -83,9 +121,21 @@ class TestFileService(unittest.TestCase):
 
         self.assertFalse(self.tm1.files.exists(self.FILE_NAME1))
 
+    @skip_if_insufficient_version(version="12")
+    def test_delete_exists_in_folder(self):
+        self.assertTrue(self.tm1.files.exists(self.FILE_NAME1_IN_FOLDER))
+
+        self.tm1.files.delete(self.FILE_NAME1_IN_FOLDER)
+
+        self.assertFalse(self.tm1.files.exists(self.FILE_NAME1_IN_FOLDER))
+
     def tearDown(self) -> None:
         if self.tm1.files.exists(self.FILE_NAME1):
             self.tm1.files.delete(self.FILE_NAME1)
         if self.tm1.files.exists(self.FILE_NAME2):
             self.tm1.files.delete(self.FILE_NAME2)
+        if self.tm1.files.exists(self.FILE_NAME1_IN_FOLDER):
+            self.tm1.files.delete(self.FILE_NAME1_IN_FOLDER)
+        if self.tm1.files.exists(self.FILE_NAME2_IN_FOLDER):
+            self.tm1.files.delete(self.FILE_NAME2_IN_FOLDER)
         self.tm1.logout()
