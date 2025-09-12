bcca85e95256fb0ec70678eaf01c0b1a614253e3
MariusWirtz <MariusWirtz2@gmail.com>
2025-09-12 10:03:21 +0200

    Fix chore tests

--- Tests/ChoreStartTime_test.py
+++ Tests/ChoreStartTime_test.py
@@ -7,24 +7,24 @@ class TestChoreStartTime(unittest.TestCase):
 
     def test_start_time_string_happy_case(self):
         chore_start_time = ChoreStartTime(year=2020, month=11, day=26, hour=10, minute=11, second=2)
-        self.assertEqual(chore_start_time.start_time_string, "2020-11-26T10:11:02Z")
+        self.assertEqual("2020-11-26T10:11:02Z", chore_start_time.start_time_string)
 
     def test_start_time_string_no_seconds(self):
         chore_start_time = ChoreStartTime(year=2020, month=11, day=26, hour=10, minute=11, second=0)
-        self.assertEqual(chore_start_time.start_time_string, "2020-11-26T10:11Z")
+        self.assertEqual("2020-11-26T10:11:00Z", chore_start_time.start_time_string)
 
     def test_start_time_string_no_minutes_no_seconds(self):
         chore_start_time = ChoreStartTime(year=2020, month=11, day=26, hour=10, minute=0, second=0)
-        self.assertEqual(chore_start_time.start_time_string, "2020-11-26T10:00Z")
+        self.assertEqual("2020-11-26T10:00:00Z", chore_start_time.start_time_string)
 
     def test_start_time_string_with_positive_tz(self):
         chore_start_time = ChoreStartTime(year=2020, month=11, day=26, hour=10, minute=1, second=1, tz="+02:00")
-        self.assertEqual(chore_start_time.start_time_string, "2020-11-26T10:01:01+02:00")
+        self.assertEqual("2020-11-26T10:01:01+02:00", chore_start_time.start_time_string)
 
     def test_start_time_string_with_negative_tz(self):
         chore_start_time = ChoreStartTime(year=2020, month=11, day=26, hour=10, minute=1, second=1, tz="-01:00")
-        self.assertEqual(chore_start_time.start_time_string, "2020-11-26T10:01:01-01:00")
+        self.assertEqual("2020-11-26T10:01:01-01:00", chore_start_time.start_time_string)
 
     def test_start_time_string_without_tz(self):
         chore_start_time = ChoreStartTime(year=2020, month=11, day=26, hour=10, minute=1, second=1)
-        self.assertEqual(chore_start_time.start_time_string, "2020-11-26T10:01:01Z")
+        self.assertEqual("2020-11-26T10:01:01Z", chore_start_time.start_time_string)
