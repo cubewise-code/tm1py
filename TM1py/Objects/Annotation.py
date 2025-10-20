28e74592dc4d99f434157ac1858ed55c55bc1945
onefloid <33193059+onefloid@users.noreply.github.com>
2025-10-20 21:44:16 +0200

    docs: Add docstrings

--- TM1py/Objects/Annotation.py
+++ TM1py/Objects/Annotation.py
@@ -29,6 +29,20 @@ class Annotation(TM1Object):
         last_updated_by: str = None,
         last_updated: str = None,
     ):
+        """
+        Initialize an Annotation object.
+
+        :param comment_value: The value of the annotation comment.
+        :param object_name: Name of the TM1 object the annotation is attached to.
+        :param dimensional_context: Iterable of dimension elements providing context.
+        :param comment_type: Type of the comment (default "ANNOTATION").
+        :param annotation_id: Unique ID of the annotation.
+        :param text: Text of the annotation.
+        :param creator: Creator of the annotation.
+        :param created: Creation timestamp.
+        :param last_updated_by: Last user who updated the annotation.
+        :param last_updated: Last update timestamp.
+        """
         self._id = annotation_id
         self._text = text
         self._creator = creator
@@ -45,7 +59,7 @@ class Annotation(TM1Object):
         """Alternative constructor
 
         :param annotation_as_json: String, JSON
-        :return: instance of TM1py.Process
+        :return: instance of Annotation
         """
         annotation_as_dict = json.loads(annotation_as_json)
         annotation_id = annotation_as_dict["ID"]
@@ -73,56 +87,112 @@ class Annotation(TM1Object):
 
     @property
     def body(self) -> str:
+        """
+        Get the annotation body as a JSON string.
+
+        :return: JSON string representation of the annotation.
+        """
         return json.dumps(self._construct_body())
 
     @property
     def body_as_dict(self) -> Dict:
+        """
+        Get the annotation body as a dictionary.
+
+        :return: Dictionary representation of the annotation.
+        """
         return self._construct_body()
 
     @property
     def comment_value(self) -> str:
+        """
+        Get the comment value.
+
+        :return: The comment value string.
+        """
         return self._comment_value
 
     @property
     def text(self) -> str:
+        """
+        Get the annotation text.
+
+        :return: The annotation text.
+        """
         return self._text
 
     @property
     def dimensional_context(self) -> List[str]:
+        """
+        Get the dimensional context.
+
+        :return: List of dimension elements providing context.
+        """
         return self._dimensional_context
 
     @property
     def created(self) -> str:
+        """
+        Get the creation timestamp.
+
+        :return: Creation timestamp as string.
+        """
         return self._created
 
     @property
     def object_name(self) -> str:
+        """
+        Get the object name.
+
+        :return: Name of the TM1 object.
+        """
         return self._object_name
 
     @property
     def last_updated(self) -> str:
+        """
+        Get the last updated timestamp.
+
+        :return: Last update timestamp as string.
+        """
         return self._last_updated
 
     @property
     def last_updated_by(self) -> str:
+        """
+        Get the last user who updated the annotation.
+
+        :return: Username of last updater.
+        """
         return self._last_updated_by
 
     @comment_value.setter
     def comment_value(self, value: str):
+        """
+        Set the comment value.
+
+        :param value: New comment value.
+        """
         self._comment_value = value
 
     @property
     def id(self) -> str:
+        """
+        Get the annotation ID.
+
+        :return: Annotation ID string.
+        """
         return self._id
 
     def move(self, dimension_order: Iterable[str], dimension: str, target_element: str, source_element: str = None):
-        """Move annotation on given dimension from source_element to target_element
+        """
+        Move annotation on given dimension from source_element to target_element.
 
-        :param dimension_order: List, order of the dimensions in the cube
-        :param dimension: dimension name
-        :param target_element: target element name
-        :param source_element:  source element name
-        :return:
+        :param dimension_order: List, order of the dimensions in the cube.
+        :param dimension: Dimension name.
+        :param target_element: Target element name.
+        :param source_element: Source element name (optional).
+        :return: None
         """
         for i, dimension_name in enumerate(dimension_order):
             if dimension_name.lower() == dimension.lower():
@@ -130,9 +200,10 @@ class Annotation(TM1Object):
                     self._dimensional_context[i] = target_element
 
     def _construct_body(self) -> Dict:
-        """construct the ODATA conform JSON represenation for the Annotation entity.
+        """
+        Construct the ODATA conform JSON representation for the Annotation entity.
 
-        :return: string, the valid JSON
+        :return: Dictionary, the valid JSON.
         """
         dimensional_context = [{"Name": element} for element in self._dimensional_context]
         body = collections.OrderedDict()
@@ -151,6 +222,12 @@ class Annotation(TM1Object):
         return body
 
     def construct_body_for_post(self, cube_dimensions) -> Dict:
+        """
+        Construct the body for POST requests to create an annotation.
+
+        :param cube_dimensions: List of cube dimension names.
+        :return: Dictionary for POST request body.
+        """
         body = collections.OrderedDict()
         body["Text"] = self.text
         body["ApplicationContext"] = [
