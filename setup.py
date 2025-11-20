9156164478a3b52775902008fc27c879926eb6fd
nicolasbisurgi <nbisurgi@cubewise.com>
2025-11-20 15:56:07 -0300

    added semantic versioning and automated releases https://github.com/cubewise-code/tm1py/discussions/1312

--- setup.py
+++ setup.py
@@ -1,54 +1,7 @@
 from setuptools import setup
 
-SCHEDULE_VERSION = "2.2"
-SCHEDULE_DOWNLOAD_URL = "https://github.com/Cubewise-code/TM1py/tarball/" + SCHEDULE_VERSION
-
-with open("README.md", "r") as f:
-    long_description = f.read()
-
+# All configuration is now in pyproject.toml
+# This setup.py is kept for backwards compatibility
 setup(
-    name="TM1py",
     packages=["TM1py", "TM1py/Exceptions", "TM1py/Objects", "TM1py/Services", "TM1py/Utils"],
-    version=SCHEDULE_VERSION,
-    description="The python module for TM1.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="MIT",
-    author="Marius Wirtz",
-    author_email="MWirtz@cubewise.com",
-    url="https://github.com/cubewise-code/tm1py",
-    download_url=SCHEDULE_DOWNLOAD_URL,
-    keywords=["TM1", "IBM Cognos TM1", "Planning Analytics", "PA", "Cognos"],
-    classifiers=[
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
-        "Natural Language :: English",
-    ],
-    install_requires=[
-        "ijson",
-        "requests",
-        "pytz",
-        'requests_negotiate_sspi;platform_system=="Windows"',
-        "mdxpy>=1.3.1",
-    ],
-    extras_require={
-        "pandas": ["pandas"],
-        "dev": [
-            "pytest",
-            "pytest-xdist",
-            "python-dateutil",
-            "black",
-            "ruff",
-        ],
-    },
-    python_requires=">=3.6",
 )
