47fb2c153eab09721556c6698dbdff4fe23587ac
MariusWirtz <MariusWirtz2@gmail.com>
2026-01-30 17:25:11 +0100

    Micro Release implement copilot feedback

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
 from setuptools import setup
 
-# All configuration is now in pyproject.toml
-# This setup.py is kept for backwards compatibility
-setup(
-    packages=["TM1py", "TM1py/Exceptions", "TM1py/Objects", "TM1py/Services", "TM1py/Utils"],
-)
+setup()
